369cbf71bad9d32766564032deea6c9f98230428
Mr.Fish <martinez.facundo85@gmail.com>
2023-10-03 11:28:02 -0300

    Inicio del proyecto

--- src/main/java/edu/unam/webappconsultoriopsico/WebappConsultorioPsicoApplication.java
+++ src/main/java/edu/unam/webappconsultoriopsico/WebappConsultorioPsicoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebappConsultorioPsicoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebappConsultorioPsicoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebappConsultorioPsicoApplication.class, args);
+    }
 
 }
--- src/test/java/edu/unam/webappconsultoriopsico/WebappConsultorioPsicoApplicationTests.java
+++ src/test/java/edu/unam/webappconsultoriopsico/WebappConsultorioPsicoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebappConsultorioPsicoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
